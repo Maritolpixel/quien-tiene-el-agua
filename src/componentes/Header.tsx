152b5fc8a31f325d7fa93923eea681c1278a63ec
Jesús Daniel Gómez Hernández <psp.dgomez@centrogeo.edu.mx>
2026-06-08 12:04:55 -0600

    agregando header y metas

---
+++ src/componentes/Header.tsx
@@ -0,0 +1,15 @@
+export function Header() {
+  return (
+    <header className="header">
+      <div className="header-inner">
+        <a
+          href="https://tirandocodigo.mx/"
+          rel="noopener noreferrer"
+          className="marca"
+        >
+          <strong>Tirando Código</strong>
+        </a>
+      </div>
+    </header>
+  );
+}
